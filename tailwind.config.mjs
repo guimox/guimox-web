789a75cbbf12c639bcaa29ee8f91abff83c3005e
gxlpes <guilhermxlopes@gmail.com>
2024-10-24 22:08:47 -0300

    feat: float animation to the hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
       colors: {
         color: '#1a75c9',
         'color-light': '#cfe4fa',
