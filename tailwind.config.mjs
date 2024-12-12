7491fe3596f4123fee25286ff056dd57aefef29e
guimox <guilhermxlopes@gmail.com>
2024-12-12 09:48:38 -0300

    feat: update design of the project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,13 +4,28 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        slideDown: {
+          '0%': { opacity: 0, transform: 'translateY(-2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        marquee: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
         },
       },
       animation: {
-        float: 'float 3s ease-in-out infinite',
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+        'slide-down': 'slideDown 0.3s ease-in-out forwards',
+        'fade-in': 'fadeIn 0.3s ease-in-out forwards',
       },
       colors: {
         color: '#1a75c9',
