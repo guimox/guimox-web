a4927463ac2a0e147da0f8be3caddc56410b22e5
guimox <guilhermxlopes@gmail.com>
2024-12-23 13:41:39 -0300

    fix: removal of reading time markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
-import { remarkReadingTime } from './remark-reading-time.mjs'
 
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
 })
